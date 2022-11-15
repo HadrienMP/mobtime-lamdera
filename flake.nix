aee3217d72537d1574b831b48d91741257c04b80
hadrienmp <github@hadrienmp.fr>
2022-11-15 12:01:43 +0100

    add nix support

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Mob Time, on lamdera";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            git-subrepo
+          ];
+        };
+      });
+}
